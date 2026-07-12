52bb00727683b78af34460781906f098c756d7a6
Aditya Jha <ajha4436@gmail.com>
2026-07-12 23:50:36 +0530

    changed scene to have a pink orange

--- BLE/src/components/layout/Scene.tsx
+++ BLE/src/components/layout/Scene.tsx
@@ -2,9 +2,9 @@ import type { FC } from 'react';
 
 const Scene: FC = () => {
   return (
-    <mesh rotation={[0.45, 0.65, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
+    <mesh>
+      <boxGeometry />
+      <meshStandardMaterial color="pink" />
     </mesh>
   );
 };
